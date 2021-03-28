eac9ae2b9fea27a7c5c9fe7dba0817fed262396c
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-03-27 19:59:14 -0600

    :tada: [ADD] Enviroment ready

---
+++ src/containers/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header'
+import Questions from '../../components/questions'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Questions />
+    </div>
+  )
+}
+
+export default Home
