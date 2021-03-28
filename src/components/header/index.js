714c9fba808f06452399ebef210bb3b10d2565f9
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-03-28 01:50:56 -0600

    :sparkles: [ADD] Login integrated

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import AuthenticationButton from '../Buttons/AuthButtons'
 import { HeaderContainer } from './styles'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Header = () => {
+  const user = useAuth0()
+  console.log(user)
   return (
     <HeaderContainer>
       <h3>Ask me</h3>
-      <div>Stats</div>
+      <div>
+        <div>Stats</div>
+        <AuthenticationButton />
+      </div>
     </HeaderContainer>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -31,7 +32,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new Dotenv()
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
